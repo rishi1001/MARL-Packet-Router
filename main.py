6a42a02bbe231e8804704058195b0936704dd137
Rishi Shah <rishi.shah10122001@gmail.com>
2022-04-30 16:23:41 +0530

    Started Work on main

---
+++ main.py
@@ -0,0 +1,36 @@
+from src.Agent import Agent
+from src.DDQN.Agent import DQN
+from src.BaseStation import BaseStation
+from src.Iot_Nodes import Iot_Nodes
+from src.Packets import packet
+
+
+# add these in constraints
+tot_episodes = 5
+tot_time = 100
+
+
+#global variables
+IOT_Nodes = []
+BaseStation = []
+
+
+
+def read_map():
+    pass
+
+
+def train():
+    for episode in range(tot_episodes):
+        for time in range(tot_time):
+
+
+
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    print("started")
+    train()
+    test()
